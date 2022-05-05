6bc021af29d3eb6218e76a7be4d3030659c8dbde
Jodie <jodie@thejuniperstudio.com>
2022-05-05 17:42:01 +0100

    initial commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,55 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
-// You can delete this file if you're not using it
+const fetch = require('node-fetch')
+const { locationCodes } = require('./libs/locationCodes')
 
-/**
- * You can uncomment the following line to verify that
- * your plugin is being loaded in your site.
- *
- * See: https://www.gatsbyjs.com/docs/creating-a-local-plugin/#developing-a-local-plugin-that-is-outside-your-project
- */
-exports.onPreInit = () => console.log("Loaded gatsby-starter-plugin")
+exports.onPreInit = () => console.log('Loaded gatsby-source-holidayextras')
+
+console.time('Create Holiday Extras GraphQL')
+
+const NODE_TYPE = 'HolidayExtras'
+
+exports.sourceNodes = async ({ actions: { createNode }, createContentDigest, createNodeId }, pluginOptions) => {
+  const products = pluginOptions.products
+
+  getProductItems = (product) => {
+    if (!product) return
+    return fetch(`https://api.holidayextras.co.uk/v1/product/${product}/lite.js?token=${pluginOptions.token}&key=${pluginOptions.key}${pluginOptions.fields && `&fields=${pluginOptions.fields}`}`)
+      .then((result) => result.json())
+      .catch((err) => {
+        return Promise.resolve([])
+      })
+  }
+
+  processProductItems = (productItems) => {
+    if (!productItems || !productItems.API_Reply || productItems.API_Reply.Error || productItems.API_Reply.Product.length === 0) return
+    productItems.API_Reply.Product.forEach((productItem) => {
+      const nodeData = Object.assign({
+        address: productItem.address,
+        airport: locationCodes(productItem.parent),
+        children: [],
+        id: createNodeId(`${NODE_TYPE}-${productItem._prodcode}`),
+        internal: {
+          type: NODE_TYPE,
+          content: JSON.stringify(productItem),
+          contentDigest: createContentDigest(productItem)
+        },
+        latitude: parseFloat(productItem.latitude),
+        logo: productItem.logo?.replace('/imageLibrary/Images', 'https://holidayextras.imgix.net/libraryimages'),
+        longitude: parseFloat(productItem.longitude),
+        name: productItem.tripappcarparkname || productItem.tripapphotelname,
+        parent: null,
+        productCode: productItem._prodcode,
+        type: productItem._prodtype
+      })
+      createNode(nodeData)
+    })
+  }
+  getProducts = (products) => {
+    const promises = products.map(async (product) => {
+      const productItems = await getProductItems(product)
+      return processProductItems(productItems)
+    })
+    return Promise.all(promises)
+  }
+  await getProducts(products)
+}
+console.timeEnd('Create Holiday Extras GraphQL')
